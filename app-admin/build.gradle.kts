cf5bdb26e8f054b6a920b491fb958d7f7ea494de
Yejiel Nehmad <yejielnehmad@users.noreply.github.com>
2026-07-14 08:52:19 -0300

    Bump DEV apps to version 195 [skip ci]

--- app-admin/build.gradle.kts
+++ app-admin/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         create("dev") {
             dimension = "distribution"
             applicationIdSuffix = ".dev"
-            versionCode = 194
+            versionCode = 195
             versionNameSuffix = "-dev"
         }
         create("beta") {
--- app-user/build.gradle.kts
+++ app-user/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         create("dev") {
             dimension = "distribution"
             applicationIdSuffix = ".dev"
-            versionCode = 194
+            versionCode = 195
             versionNameSuffix = "-dev"
         }
         create("beta") {
