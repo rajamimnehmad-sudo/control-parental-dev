501268368154554bd5c316047882cfc6e548b144
rajamimnehmad-sudo <rajamimnehmad@gmail.com>
2026-08-25 09:22:24 -0300

    test(remote): require exact accessibility and wireless routes first

--- tools/glosh-remote-spike/app/src/test/java/com/glosh/remote/spike/wizard/SettingsRouteTest.java
+++ tools/glosh-remote-spike/app/src/test/java/com/glosh/remote/spike/wizard/SettingsRouteTest.java
@@ -10,7 +10,25 @@ import org.junit.Test;
 
 public class SettingsRouteTest {
     @Test
+    public void accessibilityOpensTheExactServiceDestinationFirst() {
+        assertEquals(SettingsRoute.ACCESSIBILITY_DETAILS,
+                SettingsRoute.accessibilityActions().get(0));
+        assertEquals(
+                SettingsRoute.ACCESSIBILITY_DETAILS,
+                SettingsRoute.firstResolvable(
+                        SettingsRoute.accessibilityActions(),
+                        Set.of(SettingsRoute.ACCESSIBILITY_DETAILS)::contains));
+        assertEquals(
+                Settings.ACTION_ACCESSIBILITY_SETTINGS,
+                SettingsRoute.firstResolvable(
+                        SettingsRoute.accessibilityActions(),
+                        Set.of(Settings.ACTION_ACCESSIBILITY_SETTINGS)::contains));
+    }
+
+    @Test
     public void wirelessDebuggingUsesBestAvailableFallback() {
+        assertEquals(SettingsRoute.WIRELESS_DEBUGGING,
+                SettingsRoute.wirelessDebuggingActions().get(0));
         assertEquals(
                 SettingsRoute.WIRELESS_DEBUGGING,
                 SettingsRoute.firstResolvable(
