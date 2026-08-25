ffcf4a91057e6b2a238ea423bbc6e9b456829e15
Yejiel Nehmad <yejielnehmad@users.noreply.github.com>
2026-08-25 18:06:25 -0300

    chore(app-user): set DEV352 for accessibility refactor gate

--- app-user/build.gradle.kts
+++ app-user/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         create("dev") {
             dimension = "distribution"
             applicationIdSuffix = ".dev"
-            versionCode = 351
+            versionCode = 352
             versionNameSuffix = "-dev"
             buildConfigField("boolean", "DAG_BROWSER_V3_BRIDGE_AVAILABLE", "true")
             ndk {
