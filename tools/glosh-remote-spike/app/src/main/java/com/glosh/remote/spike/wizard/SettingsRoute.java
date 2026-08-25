870c731e1af0866c0c16c0e82487d627ec587e45
rajamimnehmad-sudo <rajamimnehmad@gmail.com>
2026-08-25 09:21:25 -0300

    feat(remote): make exact accessibility route testable

--- tools/glosh-remote-spike/app/src/main/java/com/glosh/remote/spike/wizard/SettingsRoute.java
+++ tools/glosh-remote-spike/app/src/main/java/com/glosh/remote/spike/wizard/SettingsRoute.java
@@ -7,8 +7,17 @@ import java.util.function.Predicate;
 
 public final class SettingsRoute {
     public static final String WIRELESS_DEBUGGING = "android.settings.WIRELESS_DEBUGGING_SETTINGS";
+    public static final String ACCESSIBILITY_DETAILS =
+            "android.settings.ACCESSIBILITY_DETAILS_SETTINGS";
 
     private SettingsRoute() {
+    }
+
+    public static List<String> accessibilityActions() {
+        return List.of(
+                ACCESSIBILITY_DETAILS,
+                Settings.ACTION_ACCESSIBILITY_SETTINGS,
+                Settings.ACTION_SETTINGS);
     }
 
     public static List<String> aboutPhoneActions() {
