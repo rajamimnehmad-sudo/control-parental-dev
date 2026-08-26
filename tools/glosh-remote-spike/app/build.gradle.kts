a4a477a8109f63f2b0232c2f5a38845c64146eb9
rajamimnehmad-sudo <rajamimnehmad@gmail.com>
2026-08-25 22:12:46 -0300

    build(remote): bump compact Bubble candidate to dev16

--- tools/glosh-remote-spike/app/build.gradle.kts
+++ tools/glosh-remote-spike/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.glosh.remote.spike"
         minSdk = 30
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.1.0-dev15"
+        versionCode = 16
+        versionName = "0.1.0-dev16"
 
         buildConfigField("String", "BROKER_BASE_URL", "\"$escapedBrokerBaseUrl\"")
 
