e46fe65c36b3f0134cdad7fd3e620a50d481f5d4
Yejiel Nehmad <yejielnehmad@users.noreply.github.com>
2026-08-25 21:59:24 -0300

    Build Chrome provenance gate DEV354

--- app-user/build.gradle.kts
+++ app-user/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         create("dev") {
             dimension = "distribution"
             applicationIdSuffix = ".dev"
-            versionCode = 353
+            versionCode = 354
             versionNameSuffix = "-dev"
             buildConfigField("boolean", "DAG_BROWSER_V3_BRIDGE_AVAILABLE", "true")
             ndk {
